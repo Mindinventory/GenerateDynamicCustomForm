15dbb8c3210ea4ef0d1c04d64eb7765df18617cf
Nikunj Prajapati <nikunj.prajapati@mindinventory.com>
2021-03-19 15:15:12 +0530

    Initial Commit

---
+++ GenerateDynamicCustomForm/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GenerateDynamicCustomForm
+//
+//  Created by mind-288 on 3/19/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
